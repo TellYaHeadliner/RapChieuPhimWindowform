d0a2430d2dcd0baab7fd81270b09d14085285637
Kietntk <tuankietnguyen113@gmail.com>
2024-04-05 23:49:12 +0700

    test up

---
+++ TrangChu.Designer.cs
@@ -0,0 +1,184 @@
+﻿namespace DOANMONHOC
+{
+    partial class TrangChu
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.timKiem = new System.Windows.Forms.Button();
+            this.btnTuyChon = new System.Windows.Forms.Button();
+            this.tblTuyChon = new System.Windows.Forms.GroupBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.tinTuc = new System.Windows.Forms.Panel();
+            this.tblTuyChon.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(62, 18);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(867, 31);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "hello";
+            // 
+            // timKiem
+            // 
+            this.timKiem.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.timKiem.BackgroundImage = global::DOANMONHOC.Properties.Resources.kinhlup;
+            this.timKiem.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.timKiem.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.timKiem.Location = new System.Drawing.Point(935, 11);
+            this.timKiem.Name = "timKiem";
+            this.timKiem.Size = new System.Drawing.Size(44, 44);
+            this.timKiem.TabIndex = 2;
+            this.timKiem.UseVisualStyleBackColor = false;
+            this.timKiem.Click += new System.EventHandler(this.timKiem_Click);
+            // 
+            // btnTuyChon
+            // 
+            this.btnTuyChon.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.btnTuyChon.BackgroundImage = global::DOANMONHOC.Properties.Resources.bieutuong3soctuychon;
+            this.btnTuyChon.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnTuyChon.Location = new System.Drawing.Point(12, 12);
+            this.btnTuyChon.Name = "btnTuyChon";
+            this.btnTuyChon.Size = new System.Drawing.Size(44, 44);
+            this.btnTuyChon.TabIndex = 0;
+            this.btnTuyChon.UseVisualStyleBackColor = false;
+            this.btnTuyChon.Click += new System.EventHandler(this.btnTuyChon_Click);
+            // 
+            // tblTuyChon
+            // 
+            this.tblTuyChon.Controls.Add(this.button2);
+            this.tblTuyChon.Controls.Add(this.button1);
+            this.tblTuyChon.Controls.Add(this.label2);
+            this.tblTuyChon.Controls.Add(this.label1);
+            this.tblTuyChon.Location = new System.Drawing.Point(12, 62);
+            this.tblTuyChon.Name = "tblTuyChon";
+            this.tblTuyChon.Size = new System.Drawing.Size(220, 178);
+            this.tblTuyChon.TabIndex = 3;
+            this.tblTuyChon.TabStop = false;
+            this.tblTuyChon.Visible = false;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(46, 123);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(128, 31);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Đăng xuất";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(46, 74);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(128, 31);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Tài khoản";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.label2.Location = new System.Drawing.Point(10, 39);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(88, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "0886627561";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(9, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(150, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nguyễn Tuấn Kiệt";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(410, 55);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(519, 43);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Hàng button này mọi người tùy chỉnh theo chức năng";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // tinTuc
+            // 
+            this.tinTuc.AutoScroll = true;
+            this.tinTuc.Location = new System.Drawing.Point(12, 101);
+            this.tinTuc.Name = "tinTuc";
+            this.tinTuc.Size = new System.Drawing.Size(967, 436);
+            this.tinTuc.TabIndex = 6;
+            // 
+            // TrangChu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(991, 549);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.tblTuyChon);
+            this.Controls.Add(this.timKiem);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btnTuyChon);
+            this.Controls.Add(this.tinTuc);
+            this.Name = "TrangChu";
+            this.Text = "TRANG CHỦ";
+            this.Load += new System.EventHandler(this.TrangChu_Load);
+            this.tblTuyChon.ResumeLayout(false);
+            this.tblTuyChon.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnTuyChon;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button timKiem;
+        private System.Windows.Forms.GroupBox tblTuyChon;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Panel tinTuc;
+    }
+}
+
---
+++ TrangChu.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DOANMONHOC
+{
+    public partial class TrangChu : Form
+    {
+        public TrangChu()
+        {
+            InitializeComponent();
+        }
+        private void TrangChu_Load(object sender, EventArgs e)
+        {
+            for (int i = 0; i <= 100; i++)
+            {
+                GroupBox rgb = new GroupBox();
+                rgb.Location = new System.Drawing.Point(10, 10*(i+1)+200*i);
+                rgb.Size = new System.Drawing.Size(940, 200);
+                rgb.Name = "rg/" + i;
+
+                Label label1 = new Label();
+                Label label2 = new Label();
+                label1.Size = new System.Drawing.Size(200, 25);
+                label2.Size = new System.Drawing.Size(200, 25);
+                label1.Font = new Font(label1.Font, label1.Font.Style | FontStyle.Bold);
+                label1.Font = new Font(label1.Font.FontFamily, 16);
+                label1.Text = "Lable"+i+" aaaa";
+                label2.Text = "Lable"+i+" bbbb";
+                label1.Location = new System.Drawing.Point(20, 30);
+                label2.Location = new System.Drawing.Point(20, 70);
+                label1.Name = "l1/" + i;
+                label2.Name = "l2/" + i;
+                
+                rgb.Controls.Add(label1);
+                rgb.Controls.Add(label2);
+                foreach (Control ct in rgb.Controls)
+                    ct.Click +=  GroupBox_Click;
+                rgb.Click +=  GroupBox_Click;
+
+                tinTuc.Controls.Add(rgb);
+            }
+        }
+        private void GroupBox_Click(object sender, EventArgs e)
+        {
+            Control ct = sender as Control;
+            // Xử lý logic khi người dùng nhấp vào GroupBox
+            MessageBox.Show("Bạn đã nhấp vào GroupBox! "+ ct.Name.Split('/')[1]);
+        }
+        private void timKiem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnTuyChon_Click(object sender, EventArgs e)
+        {
+                tblTuyChon.Visible = !tblTuyChon.Visible;
+        }
+    }
+}
