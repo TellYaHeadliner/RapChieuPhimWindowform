cc472e0d0119c72e3d1e3d3aa7717c7ad4d8082a
Kietntk <tuankietnguyen113@gmail.com>
2024-04-21 15:43:32 +0700

    Coppy từu GDC

---
+++ Datalayer/DMThongBao.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Model;
+
+namespace Datalayer
+{
+    public class DMThongBao:IDM<THONGBAO>
+    {
+        private CINEMA_2Entities db = new CINEMA_2Entities();
+        //Tạo phương thức static return class để không cần tạo đối tượng mới mỗi lần chạy
+        public static DMThongBao run()
+        {
+            return new DMThongBao();
+        }
+
+        //Lấy tất cả đối tượng trong table
+        public List<THONGBAO> selectAll()
+        {
+            //Lấy theo ngày đăng giảm dần
+            return (from c in db.THONGBAOs 
+                orderby c.ngayDang descending 
+                select c).ToList();
+        }
+
+        //Lấy đối tượng theo id t.id
+        public THONGBAO selectByID(THONGBAO t)
+        {
+            throw new NotImplementedException();
+        }
+
+        //Thôi vào table một đối tượng
+        public bool insert(THONGBAO t)
+        {
+            throw new NotImplementedException();
+        }
+
+        //Xóa đối tượng khỏi table
+        public bool delete(THONGBAO t)
+        {
+            throw new NotImplementedException();
+        }
+        ////Cập nhật một đối tượng có id = t.id thành t trong table
+        public bool update(THONGBAO t)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Datalayer/DMTinTuc.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.ComTypes;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using Model;
+
+namespace Datalayer
+{
+    public class DMTinTuc : IDM<TINTUC>
+    {
+        private CINEMA_2Entities db = new CINEMA_2Entities();
+        //Tạo phương thức static return class để không cần tạo đối tượng mới mỗi lần chạy
+        public static DMTinTuc run()
+        {
+            return new DMTinTuc();
+        } 
+        //Lấy tất cả đối tượng có trong table
+        public List<TINTUC> selectAll()
+        {
+            //Lấy theo ngày tạo giảm dần
+            return (from c in db.TINTUCs orderby c.ngayTao select c).ToList();
+        }
+        //lấy đối tượng theo Duyệt và trạng thái
+        public List<TINTUC> selectByDuyetTrangThai(bool daDuyet,bool? trangThai = null)
+        {
+            //Lấy theo ngày đăng giảm dần
+            //Nếu nhập trạng thái sẽ lấy kèm trạng thái
+            if(trangThai != null)
+                return (from c in db.TINTUCs
+                        where c.trangThai == trangThai && c.daDuyet == daDuyet
+                        orderby c.ngayTao descending 
+                        select c
+                        ).ToList();
+            //Không nhập sẽ chỉ lấy theo daDuyet
+            return (from c in db.TINTUCs
+                    where c.daDuyet == daDuyet
+                    orderby c.ngayTao descending
+                    select c
+                ).ToList();
+        }
+        //Lấy đối tượng theo id t.id
+        public TINTUC selectByID(TINTUC t)
+        {
+            throw new NotImplementedException();
+        }
+        //Thêm đối tượng t vào table
+        public bool insert(TINTUC t)
+        {
+            throw new NotImplementedException();
+        }
+        //Xóa đối tượng t khỏi table
+        public bool delete(TINTUC t)
+        {
+            throw new NotImplementedException();
+        }
+        //Cập nhật một đối tượng có id = t.id thành t trong table
+        public bool update(TINTUC t)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
