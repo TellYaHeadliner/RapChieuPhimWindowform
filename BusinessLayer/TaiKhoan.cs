4c00570b8a95c76ab81e53f29bcc3e5f367a3278
Kietntk <tuankietnguyen113@gmail.com>
2024-04-21 15:19:16 +0700

    Hoàn thành giao diện chung

---
+++ BusinessLayer/TaiKhoan.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Datalayer;
+using Model;
+
+namespace BusinessLayer
+{
+    public class TaiKhoan
+    {
+        //Thay đổi thuộc tính thông báo của người dùng khi nhấn xem
+        public static bool checkedThongBao(string sdt)
+        {
+            try
+            {
+                //Tạo đối tượng tài khoản có soDienThoai của người dùng
+                TAIKHOAN tk = new TAIKHOAN() { soDienThoai = sdt };
+                //Sử dụng để tìm đối tượng cần cập nhật và gán lại
+                tk = DMTaiKhoan.run().selectByID(tk);
+                //Thay đổi thuộc tính thông báo
+                tk.thongBao = false;
+                //update vào database
+                return DMTaiKhoan.run().update(tk);
+            }
+            catch (Exception e) { }
+            return false;
+        }
+        //Tìm đối tượng có sdt = số điện thoại truyền vào
+        public static TAIKHOAN selectByID(string sdt)
+        {
+            return DMTaiKhoan.run().selectByID(new TAIKHOAN(){soDienThoai = sdt});
+        }
+    }
+}
---
+++ BusinessLayer/TinTuc.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design.Serialization;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Model;
+using Datalayer;
+
+namespace BusinessLayer
+{
+    public class TinTuc
+    {
+        //Lấy tin tức
+        public static List<TINTUC> allTinTuc(bool? daDuyet = null,bool? trangThai = null)
+        {
+            //Nếu nhập đã duyệt thì sẽ tìm theo đã duyệt và trạng thái
+            if (daDuyet!=null )
+                return DMTinTuc.run().selectByDuyetTrangThai((daDuyet == true ? true : false),trangThai);
+            else
+                return DMTinTuc.run().selectAll();
+        } 
+    }
+}
